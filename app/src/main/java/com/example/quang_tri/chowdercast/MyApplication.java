a2655fd6eb604fcb6c5b0a9af059ff602a0088d1
Quang-Tri <qtrido@gmail.com>
2017-02-09 18:26:19 -0500

    *

---
+++ app/src/main/java/com/example/quang_tri/chowdercast/MyApplication.java
@@ -0,0 +1,28 @@
+package com.example.quang_tri.chowdercast;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by Quang-Tri on 04/02/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
